1171f1ac11094d75425f3bcf3c584081913105fd
priyam0803 <73890616+priyam0803@users.noreply.github.com>
2025-08-06 08:23:25 -0500

    Update load_tasty_bytes.sql

--- module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
+++ module-1/hamburg_weather/pipeline/data/load_tasty_bytes.sql
@@ -65,7 +65,7 @@ CREATE TABLE {{env}}_tasty_bytes.raw_pos.country
    iso_country VARCHAR(2),
    city VARCHAR(16777216),
    city_population VARCHAR(16777216),
-   city id number(19,0)
+   city_id NUMBER(19,0)
 );
 
 
